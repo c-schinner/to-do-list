a354ed6f97907635e167cebc535a18265a920d54
Chris <cschinner7@gmail.com>
2024-08-06 08:17:26 -0700

    add production build for deployment, and made a few minor code change on button and input size.

--- to-do-list/src/components/NewTask.jsx
+++ to-do-list/src/components/NewTask.jsx
@@ -17,22 +17,22 @@ const NewTask = ({ onSubmit }) => {
     return (
         <form onSubmit={handleSubmit} className="bg-slate-700 p-6">
             <div className="flex flex-col items-center justify-center h-full gap-4">
-            <label 
-            htmlFor="item"  
-            className="text-white text-4xl m-2">
-                New Item
-            </label>
-            <input 
-            type="text"
-            id="item"
-            value={task} 
-            onChange={(e) => setTask(e.target.value)}
-            className="bg-slate-300 text-gray-700 text-2xl p-2 rounded-md focus:outline-none hover:bg-white w-full m-2"/>
-            <button 
-            className="bg-blue-400 text-2xl p-2 m-2 w-full rounded-md border-transparent hover:text-blue-400 hover:bg-slate-600 hover:border-blue-500">
-                Add
-            </button>
-            </div>
+                <label 
+                htmlFor="item"  
+                className="text-white text-4xl m-2">
+                    New Item
+                </label>
+                <input 
+                type="text"
+                id="item"
+                value={task} 
+                onChange={(e) => setTask(e.target.value)}
+                className="bg-slate-300 text-gray-700 text-2xl p-2 max-w-[1800px] rounded-md focus:outline-none hover:bg-white m-2"/>
+                <button 
+                className="bg-blue-400 max-w-[1200px] text-2xl p-2 m-2 rounded-md border-transparent hover:text-blue-400 hover:bg-slate-600 hover:border-blue-500">
+                    Add
+                </button>
+                </div>
         </form>
  );
 };
